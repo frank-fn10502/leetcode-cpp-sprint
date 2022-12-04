459f7a8f6b6ce6e8dd6c0d61a26553196993fae7
frank-fn10502 <frank08.chen@gmail.com>
2022-12-04 08:42:38 +0000

    linear time

--- 11.container-with-most-water.cpp
+++ 11.container-with-most-water.cpp
@@ -8,11 +8,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int maxArea(vector<int>& height) {
-        
+    int maxArea(vector<int> &height)
+    {
+        int left = 0, right = height.size() - 1;
+        int ln = 0, rn = 0, result = 0;
+        while (left < right)
+        {
+            ln = height[left];
+            rn = height[right];
+
+            int h = min(ln, rn);
+            int w = right - left;
+            int area = h * w;
+            result = max(result, area);
+
+            if (ln < rn)
+                left++;
+            else if (rn < ln)
+                right--;
+            else
+            {
+                left++;
+                right--;
+            }
+        }
+        return result;
     }
 };
 // @lc code=end
-
