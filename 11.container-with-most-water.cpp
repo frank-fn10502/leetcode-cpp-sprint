cf72488b73962053af2b4d89ba0ec016ca3cc4fc
frank-fn10502 <frank08.chen@gmail.com>
2022-12-04 08:09:43 +0000

    init

---
+++ 11.container-with-most-water.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=11 lang=cpp
+ *
+ * [11] Container With Most Water
+ */
+
+// @lc code=start
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        
+    }
+};
+// @lc code=end
+
