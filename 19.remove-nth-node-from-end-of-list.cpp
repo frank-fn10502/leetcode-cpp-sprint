7bd67d0c3eab081a996f4ffde8d0cf60033f2fa7
frank-fn10502 <frank08.chen@gmail.com>
2022-12-06 16:20:22 +0000

    O(2n) solution

--- 19.remove-nth-node-from-end-of-list.cpp
+++ 19.remove-nth-node-from-end-of-list.cpp
@@ -17,20 +17,40 @@
  */
 #include <bits/stdc++.h>
 using namespace std;
-struct ListNode
-{
-    int val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
+// struct ListNode
+// {
+//     int val;
+//     ListNode *next;
+//     ListNode() : val(0), next(nullptr) {}
+//     ListNode(int x) : val(x), next(nullptr) {}
+//     ListNode(int x, ListNode *next) : val(x), next(next) {}
+// };
 
 class Solution
 {
 public:
     ListNode *removeNthFromEnd(ListNode *head, int n)
     {
+        int count = 1;
+        ListNode *current = head;
+        while (current->next != NULL)
+        {
+            current = current->next;
+            count++;
+        }
+        ListNode *preNode = NULL;
+        current = head;
+
+        while (count > n)
+        {
+            preNode = current;
+            current = current->next;
+            count--;
+        }
+        if (preNode == NULL)
+            return current->next;
+        preNode->next = current->next;
+        return head;
     }
 };
 // @lc code=end
