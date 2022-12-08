821af3b48738bc87c796ca051495cb9a6acc8639
frank-fn10502 <frank08.chen@gmail.com>
2022-12-08 18:04:03 +0000

    only use pointer & loop solution,O(n^2)

--- 23.merge-k-sorted-lists.cpp
+++ 23.merge-k-sorted-lists.cpp
@@ -15,11 +15,75 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
+#include <bits/stdc++.h>
+using namespace std;
+// struct ListNode
+// {
+//     int val;
+//     ListNode *next;
+//     ListNode() : val(0), next(nullptr) {}
+//     ListNode(int x) : val(x), next(nullptr) {}
+//     ListNode(int x, ListNode *next) : val(x), next(next) {}
+// };
+
+class Solution
+{
 public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        
+    ListNode *mergeKLists(vector<ListNode *> &lists)
+    {
+        if (lists.size() == 0)
+            return {};
+        if (lists.size() == 1)
+            return lists[0];
+
+        ListNode *list1 = nullptr, *list2 = nullptr,
+                 *result = nullptr,
+                 *current = nullptr;
+        for (int i = 0; i < lists.size(); i++)
+        {
+            if (result == nullptr)
+            {
+                result = lists[i];
+                continue;
+            }
+            if (lists[i] == nullptr)
+                continue;
+            list1 = result;                                     //集合的 list
+            list2 = lists[i];                                   // 下一個 List
+            result = (list1->val < list2->val ? list1 : list2); // 最小的起始為新的 head
+            current = nullptr;
+            while (list1 != nullptr && list2 != nullptr)
+            {
+                if (list1->val < list2->val)
+                {
+                    if (current == nullptr)
+                        current = list1;
+                    else
+                    {
+
+                        current->next = list1;
+                        current = current->next;
+                    }
+                    list1 = list1->next;
+                }
+                else
+                {
+                    if (current == nullptr)
+                        current = list2;
+                    else
+                    {
+                        current->next = list2;
+                        current = current->next;
+                    }
+                    list2 = list2->next;
+                }
+            }
+            if (list1 != nullptr)
+                current->next = list1;
+            else if (list2 != nullptr)
+                current->next = list2;
+        }
+        return result;
     }
 };
 // @lc code=end
-
